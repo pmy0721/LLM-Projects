526a677dcbda31927ad4552ab7c2dbca4a4ac422
Mekey Pan <85235079+pmy0721@users.noreply.github.com>
2025-11-04 11:16:45 +0800

    Add files via upload

---
+++ learn_boy/chatmodel_test.py
@@ -0,0 +1,7 @@
+from langchain_openai import ChatOpenAI
+
+llm = ChatOpenAI(base_url="https://api.siliconflow.cn/v1/", model="deepseek-ai/DeepSeek-V3.1-Terminus",
+                 api_key="sk-yextfaofqhhcormthytogtvtjwelbpbuzzbzzlqeeqvpfmgq")
+
+response = llm.invoke("什么是RAG")
+print(response.content)
