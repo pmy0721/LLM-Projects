7586a941eb9c82d8414bcd0d8b3b838479fce778
Mekey Pan <85235079+pmy0721@users.noreply.github.com>
2025-11-04 15:23:28 +0800

    Update chatmodel_test.py

--- learn_boy/chatmodel_test.py
+++ learn_boy/chatmodel_test.py
@@ -1,7 +1,8 @@
 from langchain_openai import ChatOpenAI
 
 llm = ChatOpenAI(base_url="https://api.siliconflow.cn/v1/", model="deepseek-ai/DeepSeek-V3.1-Terminus",
-                 api_key="sk-yextfaofqhhcormthytogtvtjwelbpbuzzbzzlqeeqvpfmgq")
+                 api_key="***")
 
 response = llm.invoke("什么是RAG")
 print(response.content)
+
